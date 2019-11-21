15cb23f9ecd1ddd4620886d4db06c22c2889484a
Andrew Shackelford <andrewshackelford97@gmail.com>
2019-11-21 14:41:16 -0500

    most of sarah's treemap vis merged, need to fix positioning of detail information

--- implementation/js/main.js
+++ implementation/js/main.js
@@ -1,7 +1,20 @@
 /* main JS file */
 
 var sentimentBubbleCloud;
+var treeMap;
 
 d3.json("data/sentiment.json", function(error, jsonData) {
-    sentimentBubbleCloud = new SentimentBubbleCloud("#bubble-cloud", jsonData);
+    if (!error) {
+        sentimentBubbleCloud = new SentimentBubbleCloud("#bubble-cloud", jsonData);
+    }
 });
+
+d3.json("data/top25Stats.json", function(error, jsonData){
+    if (!error) {
+        treeMap = new TreeMap("#treemap", jsonData)
+    }
+});
+
+function toTreemap() {
+    treeMap.toTreemap();
+}
