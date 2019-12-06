5e9350f173e657a8229a9819ddb2e70a97f97063
Andrew Shackelford <andrewshackelford97@gmail.com>
2019-12-06 16:52:53 -0500

    fixed asterplot tooltips and removed commented out code

--- implementation/js/asterplot.js
+++ implementation/js/asterplot.js
@@ -23,15 +23,9 @@ AsterPlot.prototype.initVis = function() {
     vis.radius = Math.min(vis.width, vis.height) / 2 - 50;
     vis.innerRadius = 0;
 
-    vis.tip = d3.tip()
-        .attr('class', 'd3-tip-padding')
-        .offset([10, -10])
-        .html(function(d, i) {
-            return "<span style='color:#72ffff'>" + d.data.prettyHour + "</span>" +
-                "<br>" + (d.data.total).toLocaleString() + " comments" +
-                "<br>" + "Avg. controversiality: " + d.data.controversiality.toFixed(2) +
-                "<br>" + "Avg. score: " + d.data.score.toFixed(2);
-    });
+    vis.tip = d3.select("#asterplot").append("div")
+        .attr("class", "d3-tip-aster")
+        .style("display", "none");
 
     vis.pie = d3.pie()
         .sort(null)
@@ -49,8 +43,6 @@ AsterPlot.prototype.initVis = function() {
         .attr("height", vis.height)
         .append("g")
         .attr("transform", "translate(" + vis.width / 2 + "," + vis.height / 2 + ")");
-
-    vis.svg.call(vis.tip);
 
     vis.colorScale = d3.scaleSequential(d3.interpolateBlues);
 
@@ -145,8 +137,19 @@ AsterPlot.prototype.updatePlotType = function() {
         .attr("id", function(d) {
             return "asterplot-pie-" + d.data["hour"];
         })
-        .on('mouseover', vis.tip.show)
-        .on('mouseout', vis.tip.hide)
+        .on('mouseover', function(d) {
+            vis.tip.style("display", "inline");
+            vis.tip.html("<span style='color:#72ffff'>" + d.data.prettyHour + "</span>" +
+                "<br>" + (d.data.total).toLocaleString() + " comments" +
+                "<br>" + "Avg. controversiality: " + d.data.controversiality.toFixed(2) +
+                "<br>" + "Avg. score: " + d.data.score.toFixed(2));
+        })
+        .on('mousemove', function (d) {
+            vis.tip
+                .style("left", (d3.event.x - 145) + "px")
+                .style("top", (d3.event.y - 182) + "px");
+        })
+        .on('mouseout', function() { vis.tip.style("display", "none"); })
         .on("click", function(d){
             vis.tableCreate(d)
         });
@@ -162,32 +165,32 @@ AsterPlot.prototype.updatePlotType = function() {
 
     // appending hour markers
     vis.svg.append("text")
-        .attr("x", "28")
+        .attr("x", "0")
         .attr("y", "-260")
         .attr("text-anchor", "middle")
         .attr("class", "hourText")
-        .text("12 am");
+        .text("12 AM");
 
     vis.svg.append("text")
-        .attr("x", "-40")
+        .attr("x", "0")
         .attr("y", "270")
         .attr("text-anchor", "middle")
         .attr("class", "hourText")
-        .text("12 pm");
+        .text("12 PM");
 
     vis.svg.append("text")
         .attr("x", "275")
-        .attr("y", "35")
+        .attr("y", "5")
         .attr("text-anchor", "middle")
         .attr("class", "hourText")
-        .text("6 am");
+        .text("6 AM");
 
     vis.svg.append("text")
         .attr("x", "-275")
-        .attr("y", "-30")
+        .attr("y", "5")
         .attr("text-anchor", "middle")
         .attr("class", "hourText")
-        .text("6 pm");
+        .text("6 PM");
 
 };
 
@@ -197,7 +200,6 @@ AsterPlot.prototype.tableCreate = function(d){
     var tbl  = document.createElement('table');
     tbl.className = "table table-bordered";
     tbl.style.width  = '100px';
-    // tbl.style.border = '1px solid black';
 
     var tableTitle = document.createElement('h3');
     tableTitle.appendChild(document.createTextNode("Top 5 Most Active Subreddits at " + (d.data["prettyHour"])))
